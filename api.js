8c0da14693313ba22822886744b660a46383eab9
Mani-kannan <manikannan@protonmail.com>
2018-03-11 23:21:57 +0530

    Added the api end points level changes

---
+++ api.js
@@ -0,0 +1,49 @@
+(function(){
+    var api = {};
+    var rp = require('request-promise');
+    var Promise = require("bluebird");
+    var config = require('./config.json');
+
+    api['get'] = function (endPoint, word){
+        var options = buildOptions(endPoint, word);
+       return rp(options).then(function (response){
+           return Promise.resolve(response);
+        }).catch(function(err){
+            console.error("Error occured while fetching the api - " + err.message);
+            return Promise.reject("Error");
+        })
+
+    };
+
+
+    var buildOptions = function(endPoint, word) {
+        var ep = config['endPoints'][endPoint];
+        var url = ep.url, headers, options;
+        url = replaceTokens(url, word);
+        headers = config['endPoints'][endPoint]['headers'];
+        headers['api_key'] = config['properties']['api_key'];
+        options = {
+            'uri' : url,
+            'headers' : headers,
+            'json' : true
+        };
+        return options;
+    };
+
+    function replaceTokens(url, word) {
+        var urlParams = url.split("/");
+        urlParams.forEach(function(param, index) {
+
+            if(param.startsWith('{') && param.endsWith('}')){
+                let actualParam = param.substr(1,param.length-2);
+                if(config['properties'][actualParam])
+                    urlParams[index] = config['properties'][actualParam];
+                if(actualParam === 'word')
+                    urlParams[index] = word
+            }
+        });
+        return urlParams.join("/");
+    };
+
+    module.exports = api;
+})();
